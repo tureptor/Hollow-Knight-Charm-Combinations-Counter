aa52265c63bb316e43b9856a2aafb71e165a8271
tureptor <mansooramiri@protonmail.com>
2024-05-06 00:16:18 +0100

    Rework overcharmed logic for speed and simplicity Sorting the list of charms by no. of notches guarentees that the new charm will be the biggest when adding to an existing combination. This also lets us get rid of the caching of the size of the largest notch in the combination.

--- HK_charms_combo_counter.py
+++ HK_charms_combo_counter.py
@@ -18,18 +18,18 @@ class CharmComboFinder:
 
         with open("hk_charms.csv", "r") as rawData:
             charmRows = csv.reader(rawData, delimiter=",")
-            for row in charmRows:
-                id, name, notches = int(row[0]), row[1], int(row[2])
-                self.charmNotches[id] = notches
-                self.charmName[id] = name
+            for i, row in enumerate(charmRows, 1):
+                name, notches = row[0], int(row[1])
+                self.charmNotches[i] = notches
+                self.charmName[i] = name
 
     def getCharmName(self, id):
         return self.charmName[id]
 
     def hasConflict(self, combo):
-        if {39, 40}.issubset(combo):  # combo has both grimmchild and carefree
+        if {24, 36}.issubset(combo):  # combo has both grimmchild and carefree
             return True
-        if {41, 42}.issubset(combo):  # combo has both kingsoul and voidheart
+        if {1, 42}.issubset(combo):  # combo has both kingsoul and voidheart
             return True
         return False
 
@@ -40,17 +40,15 @@ class CharmComboFinder:
             maxNotchesToConsider += max(self.charmNotches.values()) - 1
 
         partialCombos = []
-        # partialCombos[i][n] contains a list of tuples
-        # Each tuple is composed of a combination and a number
+        # partialCombos[i][n] contains a list of combinations
+        # the combinations themselves are a list of ints corresponding to the charm id
         # These combinations use exactly n notches
         # The notches within the combinations have an id <= i
-        # The number is the max number of notches used by a single charm
-        # This is used for determining overcharmed combinations.
 
         for i in range(numCharms + 1):
             partialCombos.append([[] for n in range(maxNotchesToConsider + 1)])
 
-        partialCombos[0][0].append(([], 0))
+        partialCombos[0][0].append([])
         # If we have no notches and no charms available, we can always use the empty set
         # The algorithm will build all other combinations based off this initial entry.
 
@@ -60,24 +58,15 @@ class CharmComboFinder:
         ):
             charmN = self.charmNotches[i]
             partialCombos[i][n] += [c for c in partialCombos[i - 1][n]]
-            if charmN + n <= self.numNotches:
+            if (charmN + n <= self.numNotches) or (
+                self.allowOvercharmed and n < self.numNotches
+            ):
                 for prev in partialCombos[i - 1][n]:
-                    partialCombos[i][n + charmN].append(
-                        (prev[0] + [i], max(prev[1], charmN))
-                    )
-            elif self.allowOvercharmed:
-                for prev in partialCombos[i - 1][n]:
-                    if n < self.numNotches or n + charmN - prev[1] < self.numNotches:
-                        # either there is an empty notch in the existing combination
-                        # or we can take the biggest charm out (-prev[1]), put the current charm in (+charmN)
-                        # and this would leave empty notches for the biggest charm to put back in
-                        partialCombos[i][n + charmN].append(
-                            (prev[0] + [i], max(prev[1], charmN))
-                        )
+                    partialCombos[i][n + charmN].append(prev + [i])
 
         print("finished generation of combos")
         for w in partialCombos[numCharms]:
-            for combo, _ in w:
+            for combo in w:
                 if not self.hasConflict(combo):
                     yield combo
 
